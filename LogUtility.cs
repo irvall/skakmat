837e347c4d5266c344b6fbdbaca852700563cafc
Johan Irvall Rasmussen <jra@immeo.dk>
2023-11-20 09:36:19 +0100

    updated ignore

--- LogUtility.cs
+++ LogUtility.cs
@@ -1,24 +1,23 @@
 using System.Runtime.InteropServices;
 using Raylib_cs;
 
+namespace chess_rts;
+
 public class Log
 {
-
-    public static void CustomLog(int logType, string text, IntPtr args) { }
+    private static void CustomLog(int logType, string text, IntPtr args) { }
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void TraceLogDelegate(int logType, string text, IntPtr args);
+    private delegate void TraceLogDelegate(int logType, string text, IntPtr args);
 
     public static void IgnoreRaylibLogs()
     {
-        var traceLog = new Log.TraceLogDelegate(Log.CustomLog);
+        var traceLog = new TraceLogDelegate(CustomLog);
         unsafe
         {
             Raylib.SetTraceLogCallback((delegate* unmanaged[Cdecl]<int, sbyte*, sbyte*, void>)Marshal.GetFunctionPointerForDelegate(traceLog));
         }
     }
-
-
 
     public static void WriteColor(string text, ConsoleColor consoleColor, bool newLine = true)
     {
@@ -35,8 +34,5 @@ public class Log
         Console.ForegroundColor = previousColor;
     }
 
-    public static string BoldText(string text)
-    {
-        return $"\u001b[1m{text}\u001b[0m";
-    }
+    public static string BoldText(string text) => $"\u001b[1m{text}\u001b[0m";
 }
